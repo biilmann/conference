75051636ec445e9cd4e2998ed5c3c22b54293a5f
Mathias Biilmann <info@mathias-biilmann.net>
2014-07-09 19:26:36 +0200

    Added BobFunk as a speaker

--- docpad.js
+++ docpad.js
@@ -68,7 +68,7 @@ module.exports = {
         time: "9h00"
       },
       {
-        name: "Linus Torvalds",
+        name: "Bob Funk",
         photo: "themes/yellow-swan/img/speaker.jpg",
         bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
         company: "Linux Foundation",
